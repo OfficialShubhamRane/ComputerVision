0fa29bb8b7cee1ec5e2469f92071e720c67deb13
Shubham Rane <official.shubhamrane@gmail.com>
2020-12-12 17:01:02 -0500

    Adding Shapes

--- chapter1.py
+++ chapter1.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # Reading Image from location on local machine
 # img = cv2.imread("C:/Users/Shubham/Pictures/Desktop_Bg/Formals.jpg");
@@ -13,16 +14,37 @@ import cv2
 #     if cv2.waitKey(1) & 0xFF == ord('q'):
 #         break
 
+#Using laptop camera
+# vid = cv2.VideoCapture(0)
+# vid.set(3, 640)
+# vid.set(4, 480)
+#
+# while True:
+#     success, frame = vid.read()
+#     cv2.imshow("Video", frame)
+#     # cv2.waitKey(5000)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 
-vid = cv2.VideoCapture(0)
-vid.set(3, 640)
-vid.set(4, 480)
+#Resizing and crop image
+# img = cv2.imread("C:/Users/Shubham/Pictures/Desktop_Bg/Formals.jpg")
+# print(img.shape)
+# cv2.imshow("Image", img) #Orignal Image
+#
+# imgResize= cv2.resize(img,(800,1000))   #Resized Image
+# cv2.imshow("Resized", imgResize)
+#
+# imgCropped = img[0:500, 450:800]
+# cv2.imshow("Cropped Img", imgCropped)
+# cv2.waitKey(0)
 
-while True:
-    success, frame = vid.read()
-    cv2.imshow("Video", frame)
-    # cv2.waitKey(5000)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#Text and Shapes
+img = np.zeros((512,512,3),np.uint8)
+# img[:] = 255,0,0
 
+cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(255,0,0),2)
+cv2.rectangle(img,(200,200),(400,400),((0,0,255)),(2))
+cv2.putText(img,"Hello", (300,100),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0), 1)
 
+cv2.imshow("Image",img)
+cv2.waitKey(0)
