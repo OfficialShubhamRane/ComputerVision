5bd501a45839aa59d80911b9b68ceeee40bd17d6
Shubham Rane <official.shubhamrane@gmail.com>
2020-12-17 00:29:36 -0500

    Correcting files client and server

--- Socket_Programming/client.py
+++ Socket_Programming/client.py
@@ -1,17 +1,27 @@
 import socket
 
+HEADER_SIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 1234))
 
-#RECIEVING A MESSAGE
-# msg = s.recv(1024)
-# print(msg.decode('utf-8'))
-
-# while True:
-full_msg = ''
 while True:
-    msg = s.recv(8)
-    if len(msg) == 0:
-        break
-    full_msg += msg.decode('utf-8')
-print(full_msg)
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"new message length: {msg[:HEADER_SIZE]}")
+            msglen = int(msg[:HEADER_SIZE])
+
+            new_msg = False
+
+        full_msg += msg.decode('utf-8')
+
+        if len(full_msg) - HEADER_SIZE == msglen:
+            print("Full msg received")
+            print(full_msg[HEADER_SIZE:])
+            new_msg = True
+            full_msg = ""
+
+    print(full_msg)
--- Socket_Programming/server.py
+++ Socket_Programming/server.py
@@ -1,5 +1,7 @@
 import socket
 
+HEADER_SIZE = 10
+
 #CREATING SOCKET AND GIVING IT TYPE
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -13,8 +15,12 @@ while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established")
     # SENDING A MESSAGE
-    clientsocket.send(bytes("Hello client","utf-8"))
 
-    print("Terminating Connection")
-    s.close()
+    msg = "Msg with header"
+    msg = f'{len(msg) :< {HEADER_SIZE}}' + msg
+
+    clientsocket.send(bytes(msg,"utf-8"))
+
+    # print("Terminating Connection")
+    #clientsocket.close()
 
